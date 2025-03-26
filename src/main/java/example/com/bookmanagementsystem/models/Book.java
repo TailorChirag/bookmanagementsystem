9e348b3055e92ed7c57975b3e826ba8cbe6b0b34
Chirag Tailor <vermachirag4254@gmail.com>
2025-03-26 16:28:19 +0530

    made changes in book class for Title and Author should be non-empty strings.

--- src/main/java/example/com/bookmanagementsystem/models/Book.java
+++ src/main/java/example/com/bookmanagementsystem/models/Book.java
@@ -1,6 +1,9 @@
 package example.com.bookmanagementsystem.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,9 +16,23 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @NotBlank(message = "Title cannot be empty")
+    @Size(min = 1, max = 255, message = "Title length must be between 1 and 255 characters")
     private String title;
+
+    @NotBlank(message = "Author cannot be empty")
+    @Size(min = 1, max = 255, message = "Author length must be between 1 and 255 characters")
     private String author;
+
+    @NotBlank(message = "Genre cannot be empty")
     private String genre;
-    private boolean available;  // true = available, false = not available
+
+    @Enumerated(EnumType.STRING)
+    @NotNull(message = "Availability status cannot be null")
+    private AvailabilityStatus availabilityStatus;
+
+    public enum AvailabilityStatus {
+        AVAILABLE,
+        CHECKED_OUT
+    }
 }
