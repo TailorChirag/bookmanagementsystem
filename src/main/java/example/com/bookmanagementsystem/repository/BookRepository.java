b58fb60c65a67883392d6a0e6170100f93d9c415
Chirag Tailor <vermachirag4254@gmail.com>
2025-03-26 16:31:04 +0530

    Added existsByTitle() to check for duplicate titles and Used Optional to handle null values safely.

--- src/main/java/example/com/bookmanagementsystem/repository/BookRepository.java
+++ src/main/java/example/com/bookmanagementsystem/repository/BookRepository.java
@@ -5,8 +5,14 @@ import example.com.bookmanagementsystem.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+
     List<Book> findByTitleContainingIgnoreCase(String title);
+
+    Optional<Book> findById(Long id);
+
+    boolean existsByTitle(String title);
 
 }
